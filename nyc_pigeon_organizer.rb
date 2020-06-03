8677ec963618b0ff811879e05522a93192d21e2b
Aian Soktoev <soktoevayan@gmail.com>
2020-06-03 20:38:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,52 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+    },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+    },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  final_results = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key.to_s)
+      end
+    end
+  end
+end  
+
+#def nyc_pigeon_organizer(data)
+  #newArr = []
+  #data.each do |key, value|
+    #value.each do |key1, value1|
+      #newArr.push(value1)
+    #end   
+  #end
+  #puts a = newArr.flatten.uniq  
+#end
+
+#nyc_pigeon_organizer(pigeon_data)
+
+#def create_new_hash(data, a?)
+  #data.map do |key, value|
+    #a.each do |element|
+      #key = element
+  
