ea1bcbb3bd43ba6d5c97b19e48fe496e14e752f5
Daniel Hahm <dhahm01@gmail.com>
2020-06-11 21:28:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  result = data.each_with_object({}) do |(key, value), final_output|
+    value.each do |key2, value2|
+      value2.each do |name|
+        if !final_output[name]
+          final_output[name] = {}
+        end
+        if !final_output[name][key]
+          final_output[name][key] = []
+        end
+        final_output[name][key].push(key2.to_s)
+      end
+    end
+  end 
+  result
 end
