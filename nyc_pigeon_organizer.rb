b23033dca5317b121c4273dfb253fde5f30bd6e9
Duke Ko <dukhyunko93@gmail.com>
2020-06-12 14:21:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,21 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_array|
+  final_results = {}
+  data.each do |key, value|
     value.each do |inner_key, names|
       names.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
+
+        if !final_results[name]
+          final_results[name] = {}
         end
-        if !final_array[name][key]
-          final_array[name][key] = []
+
+        if !final_results[name][key]
+          final_results[name][key] = []
         end
-        final_array[name][key].push(inner_key.to_s)
+
+        final_results[name][key].push(inner_key.to_s)
+
       end
     end
   end
+  final_results
 end
