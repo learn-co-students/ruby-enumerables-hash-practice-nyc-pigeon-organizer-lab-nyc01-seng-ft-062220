ddeb0a1a4e57ef7e50cd545e0b259239e3e62dca
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-15 03:03:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  bird_names = Array.new
+  traits = Array.new
+  descriptions = Array.new
+  answer = Hash.new
+
+
+#cycles thru the hash and takes the birds and associates them the 1st level keys
+  data.each do |key,value|
+    traits << key
+    value.each do |description,birds|
+      descriptions << description
+      birds.each do |i|
+        if answer.include?(i)
+          answer[i][key] = []
+        else
+          answer[i]= {}
+          answer[i][key]= []
+        end
+      end
+    end
+  end
+
+#This will cycle thru the existing hash and append the descriptions to the new hash created above
+  answer.select do |bird,trait|
+    data.each do |first_key,first_value|
+      first_value.each do |second_key,second_value|
+
+
+            if second_value.include?(bird)
+
+              answer[bird][first_key]<<second_key.to_s
+            end
+      end
+    end
+  end
+
 end
